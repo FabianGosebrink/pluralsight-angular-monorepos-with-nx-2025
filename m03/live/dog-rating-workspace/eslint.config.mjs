6ade2b0e10c509e3924a54fd765e268577fde07e
FabianGosebrink <fabian.gosebrink@outlook.com>
2025-08-31 23:12:32 +0200

    fix: add tags to project.json files for better categorization and dependency management

--- m03/live/dog-rating-workspace/eslint.config.mjs
+++ m03/live/dog-rating-workspace/eslint.config.mjs
@@ -17,8 +17,45 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'scope:dog-rating-app',
+              onlyDependOnLibsWithTags: [
+                'scope:dogs',
+                'scope:about',
+                'scope:shared',
+              ],
+            },
+            {
+              sourceTag: 'scope:about',
+              onlyDependOnLibsWithTags: ['scope:shared', 'scope:about'],
+            },
+            {
+              sourceTag: 'scope:dogs',
+              onlyDependOnLibsWithTags: ['scope:shared', 'scope:dogs'],
+            },
+            {
+              sourceTag: 'scope:shared',
+              onlyDependOnLibsWithTags: ['scope:shared'],
+            },
+
+            {
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: [
+                'type:container',
+                'type:util',
+                'type:ui',
+              ],
+            },
+            {
+              sourceTag: 'type:container',
+              onlyDependOnLibsWithTags: ['type:domain', 'type:ui', 'type:util'],
+            },
+            {
+              sourceTag: 'type:ui',
+              onlyDependOnLibsWithTags: ['type:util', 'type:domain'],
+            },
+            {
+              sourceTag: 'type:domain',
+              onlyDependOnLibsWithTags: ['type:util'],
             },
           ],
         },
